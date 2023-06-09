8469c0b0fe3cf044fea3fd2fe7e3fd1584328d4b
Eric Doering <ericbenjamindoering@gmail.com>
2023-06-08 21:06:14 -0400

    updated June 8 2023

--- parse_nested_data.js
+++ parse_nested_data.js
@@ -57,4 +57,38 @@ function swapNameAndOwner(petData){
     return petData
 };
 
-console.log(swapNameAndOwner(petsOfOwners));
+// Add age to each pet with age set to the number of letter of the pet name
+
+function addAge(petData){
+    for(let i = 0; i < petData.length; i++){
+        petData[i].pet.age = petData[i].pet.name.length
+    }
+    return petData
+};
+
+// Find total added age of each animal
+
+let petsOfOwners2 = [
+    { owner : "Mike", pet: { animal: "dog", name: "Bradley", age: 7 }},
+    { owner : "Lindsey", pet: { animal: "cat", name: "Sophia", age: 6 }},
+    { owner : "Ron", pet: { animal: "bird", name: "Fay", age: 3 }},
+    { owner : "Sarah", pet: { animal: "cat", name: "Mijo", age: 4 }},
+    { owner : "Ryan", pet: { animal: "rabbit", name: "Rose", age: 4 }},
+    { owner : "Dave", pet: { animal: "dog", name: "Quinn", age: 5 }},
+    { owner : "Michelle", pet: { animal: "dog", name: "Marco", age: 5 }}
+];
+
+function addAnimalAges(petData){
+    let cumulativeAnimalAges = {};
+    for(let i = 0; i < petData.length; i++){
+        if(cumulativeAnimalAges[petData[i].pet.animal]){
+            cumulativeAnimalAges[petData[i].pet.animal] += petData[i].pet.age
+        }
+        else {
+            cumulativeAnimalAges[petData[i].pet.animal] = petData[i].pet.age;
+        }
+    }
+    return cumulativeAnimalAges
+};
+
+console.log(addAnimalAges(petsOfOwners2));
